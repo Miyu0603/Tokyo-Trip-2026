de7271da2247ba87ad089e9b3aefbe1ccc815b2d
Miyu0603 <sandy060321@gmail.com>
2025-12-19 15:42:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Crucial for GitHub Pages relative path support
+  base: '/Tokyo-Trip-2026/', // 改成你的專案名稱，前後都要有斜線
   build: {
     outDir: 'dist',
   }
